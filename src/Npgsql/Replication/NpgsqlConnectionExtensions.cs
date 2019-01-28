fe58d1a1a6adb00304fee5d2475a172144482374
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2019-01-28 03:40:22 +0000

    Add a failing test ensuring that an ArgumentNullException is thrown when a null NpgsqlConnection is passed to the CreateReplicationConnection extension method.

---
+++ src/Npgsql/Replication/NpgsqlConnectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Npgsql.Replication
+{
+    /// <summary>
+    /// Extensions for the <see cref="NpgsqlConnection"/> type pertaining to PostGres replication.
+    /// </summary>
+    public static class NpgsqlConnectionExtensions
+    {
+        /// <summary>
+        /// Test
+        /// </summary>
+        /// <param name="connection"></param>
+        public static void CreateReplicationConnection(this NpgsqlConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
